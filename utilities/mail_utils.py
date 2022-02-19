ba69841b5c7983ceeb5e477d53e3e554984fa95f
alonkl <alonkl@post.bgu.ac.il>
2022-02-19 21:09:38 +0200

    new version

--- utilities/mail_utils.py
+++ utilities/mail_utils.py
@@ -3,7 +3,6 @@ import smtplib
 
 
 def send_mail(email: str, subject: str, message: str):
-    print()
     server = smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT)
     server.starttls()
     server.login(settings.CONTACT_EMAIL, settings.CONTACT_EMAIL_PASSWORD)
